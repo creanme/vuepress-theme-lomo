16932b62a720fcfb26931bcc814ae0faf855d34d
creanme <creanme@live.com>
2020-01-22 11:33:57 +0800

    feat: 添加了页面的背景色

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -20,7 +20,7 @@ export default {
     components: {
         DefaultGlobalLayout: GlobalLayout,
         Header,
-        MobileHeader,
+        MobileHeader
     },
 
     data() {
@@ -38,11 +38,16 @@ export default {
 </script>
 
 <style lang="stylus">
-.content-wrapper {
-    padding: 160px 15px 80px 15px;
-    min-height: calc(100vh - 80px - 60px - 160px);
-    max-width: $contentWidth;
-    margin: 0 auto;
+#vuepress-theme-blog__global-layout {
+    background: linear-gradient(45deg, #cff, #ffc);
+    word-wrap: break-word;
+
+    .content-wrapper {
+        padding: 160px 15px 80px 15px;
+        min-height: calc(100vh - 80px - 60px - 160px);
+        max-width: $contentWidth;
+        margin: 0 auto;
+    }
 }
 
 @media (max-width: $MQMobile) {
